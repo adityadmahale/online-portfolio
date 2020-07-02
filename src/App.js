d975796a180bf4898c62509766494538b71280d9
Aditya Mahale <aditya.mahale@enterprisedb.com>
2020-07-02 16:16:44 +0530

    Fix navigation bar collapse issue

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 import HeroImage from "./components/heroImage";
+import NavBar from "./components/navBar";
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "jquery/dist/jquery.min.js";
+import "bootstrap/dist/js/bootstrap.min.js";
 
 function App() {
-  return <HeroImage />;
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <HeroImage />
+    </BrowserRouter>
+  );
 }
 
 export default App;
