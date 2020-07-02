4a6716f673defe47a3c7f7b19b4860285223e2a0
Aditya Mahale <aditya.mahale@enterprisedb.com>
2020-07-02 14:32:07 +0530

    Initial commit

---
+++ src/components/heroImage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HeroImage = () => {
+  return (
+    <div className="jumbotron">
+      <div className="hero-text">
+        <h1 style={{ fontSize: "5rem" }}>Aditya Mahale</h1>
+        <p>Software Engineer</p>
+      </div>
+    </div>
+  );
+};
+
+export default HeroImage;
