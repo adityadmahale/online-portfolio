d91ff49543f07e5cb9bc1b79b3634c73b7cc98c3
Aditya Mahale <aditya.mahale@enterprisedb.com>
2020-07-03 10:47:53 +0530

    Extract navbar component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,43 +1,14 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 import HeroImage from "./heroImage";
+import NavBar from "./navBar";
 
-const NavBar = () => {
-  const links = ["About", "Project", "Skills", "Contact"];
-
+const Header = () => {
   return (
     <header>
-      <nav className="navbar navbar-expand-lg fixed-top navbar-dark">
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            {links.map((link) => {
-              return (
-                <NavLink
-                  key={link}
-                  className="nav-item nav-link text-light px-4 py-3"
-                  to="#"
-                >
-                  {link}
-                </NavLink>
-              );
-            })}
-          </ul>
-        </div>
-      </nav>
+      <NavBar />
       <HeroImage />
     </header>
   );
 };
 
-export default NavBar;
+export default Header;
