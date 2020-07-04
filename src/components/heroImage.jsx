c9af0e26332d1540fa16a70c2a2d6cc529cab855
Aditya Mahale <aditya.mahale@enterprisedb.com>
2020-07-04 14:14:10 +0530

    Add chevron

--- src/components/heroImage.jsx
+++ src/components/heroImage.jsx
@@ -6,6 +6,7 @@ const HeroImage = () => {
       <div className="hero-text">
         <h1 style={{ fontSize: "5rem" }}>Aditya Mahale</h1>
         <p>Software Engineer</p>
+        <i className="fa fa-chevron-down mt-2" aria-hidden="true"></i>
       </div>
     </div>
   );
