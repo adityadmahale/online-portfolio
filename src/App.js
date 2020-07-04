c9fb346ea385eb6cf626795438dbec63a7772571
Aditya Mahale <aditya.mahale@enterprisedb.com>
2020-07-04 20:04:12 +0530

    Remove App.css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import Header from "./components/header";
 import Body from "./components/body";
 import Footer from "./components/footer";
-import "./App.css";
 
 function App() {
   return (
