b585ea9c4380a74bff32d3e48b0598420a8fa65f
Aditya Mahale <aditya.mahale@enterprisedb.com>
2020-07-07 18:57:38 +0530

    Add linkedin badge in contact section

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -6,6 +6,22 @@ const Contact = () => {
       <h1 id="#Contact" className="gold-text">
         Contact
       </h1>
+      <div
+        class="LI-profile-badge"
+        data-version="v1"
+        data-size="medium"
+        data-locale="en_US"
+        data-type="horizontal"
+        data-theme="light"
+        data-vanity="aditya-mahale"
+      >
+        <a
+          class="LI-simple-link"
+          href="https://in.linkedin.com/in/aditya-mahale?trk=profile-badge"
+        >
+          Aditya Mahale
+        </a>
+      </div>
     </React.Fragment>
   );
 };
